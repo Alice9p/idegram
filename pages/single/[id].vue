07fb5284d8b1eb2e49f271b5de4bf9b8a7b0eef7
Alicia Molinero <aliciamc7995@gmail.com>
2023-03-21 11:49:35 +0100

    Primer commit

---
+++ pages/single/[id].vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Esto en el articulo numero: {{ myId }}</h1>
+  </div>
+</template>
+<script setup>
+const route = useRoute()
+const myId = ref(route.params.id);
+
+</script>
